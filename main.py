2f857a9029ff9ba6678250690e9fba80fb441eea
Pierre-Jean Lefort <pjgame84@gmail.com>
2022-03-31 13:20:34 +0200

    working but rate limit at 5 request in 5 seconds

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 import os
+import requests
+import time
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -19,9 +22,23 @@ async def on_message(message):
 		return
 
 	if message.content.startswith('$hello'):
-		# activity = discord.CustomActivity(name="with the API", emoji=client.get_emoji(":robot:"))
-		activity = discord.Streaming(name="with the API", url="https://twitch.tv/robot")
-		print(activity, flush=True)
-		await client.change_presence(activity=activity, afk=False)
+		print("[+]: Status Changer Loaded")
+		headers = {
+			'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
+			'Content-Type': 'application/json',
+			'Authorization': os.getenv('DISCORDTOKEN'),
+    }
+		request = requests.Session()
+		while True:
+			now = datetime.now()
+			current_time = now.strftime("%H:%M:%S")
+			setting = {
+					'status': "online",
+					"custom_status": {"text": f"[+]: My current time: {current_time}"}
+					# "custom_status": {"text": "hello"}
+			}
+			res = request.patch("https://canary.discordapp.com/api/v6/users/@me/settings",headers=headers, json=setting, timeout=10)
+			print(res.headers)
+			time.sleep(1)
 		
 client.run(os.getenv('DISCORDTOKEN'), bot=False)
